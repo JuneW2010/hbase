0fae309a49edfefa203914a76a5782cdad1aebbc
Karthik K <oss.akk@gmail.com>
2010-04-29 17:21:05 -0700

    current Write Buffer Size - also add the openbitset

--- src/java/org/apache/hadoop/hbase/client/HTable.java
+++ src/java/org/apache/hadoop/hbase/client/HTable.java
@@ -679,6 +679,7 @@ public class HTable {
       OpenBitSet docs = this.termDocs.get(row);
       if (docs == null) { 
         docs = HBaseneUtil.createDefaultOpenBitSet(docId);
+        currentWriteBufferSize += (docs.getNumWords() * 8);
       }
       docs.set(docId);
       this.termDocs.put(row, docs);
